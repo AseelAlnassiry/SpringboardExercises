e8958cb77b63383a5b9e0884ff93d4631170a074
Aseel Alnassiry <aseelalnassiry@gmail.com>
2023-06-06 17:58:40 -0700

    24.1 - 15 started

---
+++ 24.1 - 15/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, session, request, render_template, flash
+from models import db, connect_db
+
+app = Flask(__name__)
+app.secret_key = "PROWLERCAR"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://adoption"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["DEBUT_TB_INTERCEPT_REDIRECTS"] = False
+app.config["SQLALCHEMY_ECHO"] = False
+app.config["SQLALCHEMY_RECORD_QUERIES"] = True
+
+connect_db(app)
+
+
+@app.route("/")
+def index():
+    return "hi there I work"
+

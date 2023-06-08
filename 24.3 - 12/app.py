d204e19ca3bd58347863499c05297ba8116a1663
Aseel Alnassiry <aseelalnassiry@gmail.com>
2023-06-08 15:01:44 -0700

    24.3 - 12 progress 2

--- 24.3 - 12/app.py
+++ 24.3 - 12/app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, render_template, request
 from models import Cupcake, connect_db, db
 
 app = Flask(__name__)
+app.app_context().push()
 app.secret_key = "PROWLERCAR"
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///cupcakes"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -17,3 +18,27 @@ def get_all_cupcakes():
     res = Cupcake.query.all()
     serialized = [Cupcake.serialize(d) for d in res]
     return jsonify(cupcakes=serialized)
+
+
+@app.post("/api/cupcakes")
+def add_one_cupcake():
+    flavor = request.form["flavor"]
+    size = request.form["size"]
+    rating = request.form["rating"]
+    image = request.form["image"]
+
+    if image:
+        new_cupcake = Cupcake(flavor=flavor, size=size, rating=rating, image=image)
+    else:
+        new_cupcake = Cupcake(flavor=flavor, size=size, rating=rating)
+    db.session.add(new_cupcake)
+    db.session.commit()
+    serialized = Cupcake.serialize(new_cupcake)
+    return jsonify(cupcake=serialized)
+
+
+@app.get("/api/cupcakes/<int:id>")
+def get_one_cupcake(id):
+    res = Cupcake.query.get(id)
+    serialized = Cupcake.serialize(res)
+    return jsonify(cupcake=serialized)
